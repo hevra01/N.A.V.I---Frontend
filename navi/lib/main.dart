bb4880c6bc168d7cfe3b3f85d8ed1c2ec74c47ce
Hevra Petekkaya <hevrapetekkaya1@gmail.com>
2022-12-27 20:54:36 +0200

    frontend is able to send frames periodically to the backend and get responses

--- navi/lib/main.dart
+++ navi/lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 //import 'package:my_first_flutter_project/pages/home.dart';
 import 'package:navi/pages/navigation.dart';
 
-// Organize the routings
-void main() => runApp(MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Navigation(),
-      },
-    ));
+List<CameraDescription>? cameras;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Navigation(),
+    },
+  ));
+}
--- navi/lib/pages/navigation.dart
+++ navi/lib/pages/navigation.dart
@@ -1,13 +1,18 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:camera/camera.dart';
 import 'package:http/http.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:navi/main.dart';
+import 'package:navi/main.dart';
 import '../API/api.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:dio/dio.dart' as eos;
 import 'package:path/path.dart';
 import 'package:http/http.dart' as http;
+
+import 'package:navi/main.dart';
 
 class Navigation extends StatefulWidget {
   const Navigation({Key? key}) : super(key: key);
@@ -19,6 +24,36 @@ class Navigation extends StatefulWidget {
 class _NavigationState extends State<Navigation> {
   // to capture frames
   final ImagePicker _picker = ImagePicker();
+  CameraController? controller;
+  File? pictureFile;
+
+  // initstate runs everytime the widget gets created
+  // but it doesn't run when it is updated.
+  @override
+  void initState() {
+    super.initState();
+    // initialize the camera controller
+    controller = CameraController(
+      cameras![0], // get the first available
+      ResolutionPreset.max,
+    );
+
+    // check if camera is initialized
+    controller?.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+
+      setState(() {});
+    });
+  }
+
+  // when the widget dies, this function is called.
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +64,6 @@ class _NavigationState extends State<Navigation> {
     // this will be updated based on the return value of server that is performing
     // scene description.
     List<List> scene = [[], [], []];
-
-    final ImagePicker _picker = ImagePicker();
 
     return Scaffold(
         appBar: AppBar(
@@ -118,29 +151,35 @@ class _NavigationState extends State<Navigation> {
   // server to stop the navigation that includes object detection,
   // distance and position calculation.
   handle_scene_description() async {
-    //var yes = ImageSource.camera;
-    //final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
-    //File photofile = File(photo!.path);
+    // the return value is a XFile
+    final picture = await controller?.takePicture();
+    var stream, length;
 
-    //var stream = http.ByteStream(photofile.openRead());
-    //stream.cast();
-    //var length = await photofile.length();
+    // get the requirments ready to create a multipart file to be sent with the post request
+    if (picture != null) {
+      pictureFile = File(picture.path);
+      stream = http.ByteStream(pictureFile!.openRead());
+      stream.cast();
+    }
+
+    var multipart =
+        await http.MultipartFile.fromPath('frame', pictureFile!.path);
 
     // If you want to send images/videos/files to the server, use MultipartRequest
-    //var request = http.MultipartRequest(
-    //    'POST', Uri.parse('http://10.0.2.2:5000/predict'));
-
-    //var multiport = new http.MultipartFile('photofile', stream, length);
-
-    var response2 = await CallApi().getData('predict');
-    print(response2);
+    var request = http.MultipartRequest(
+        'POST', Uri.parse('http://10.0.2.2:5000/predict'));
 
     // adding the image file
-    //request.files.add(multiport);
-    //var response = await request.send();
-    print("yes");
-    //setState(() {
-    //  scene = response;
-    //});
+    request.files.add(multipart);
+    // send the api request for object detection
+    var streamedResponse = await request.send();
+
+    // converting the streamed response to a casual response
+    var response = await http.Response.fromStream(streamedResponse);
+    var objects_with_positions =
+        (json.decode(response.body))["objects_with_positions"];
+    print(objects_with_positions);
+    print(objects_with_positions[1]);
+    print(objects_with_positions[3]);
   }
 }
