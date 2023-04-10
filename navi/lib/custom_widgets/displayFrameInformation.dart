25aa0a08a8f713f9126dfed5075a43353f4e86b9
Hevra Petekkaya <hevrapetekkaya1@gmail.com>
2023-04-10 11:13:57 +0300

    semantics have been integrated to the scene description

--- navi/lib/custom_widgets/displayFrameInformation.dart
+++ navi/lib/custom_widgets/displayFrameInformation.dart
@@ -1,41 +1,58 @@
 import 'package:flutter/material.dart';
 
 class FrameInformation extends StatelessWidget {
-  // this list includes a list for the detected objects,
-  // another for their distances, and a third one for their positions.
   final List sceneDescription;
 
-  const FrameInformation({super.key, required this.sceneDescription});
+  const FrameInformation({Key? key, required this.sceneDescription})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        for (var i = 0; i < (sceneDescription[0]).length; i++)
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(sceneDescription[0][i],
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
-                  )),
-              Text((sceneDescription[1][i].toStringAsFixed(2)),
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
-                  )),
-              Text((sceneDescription[2][i]).toString(),
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
-                  ))
-            ],
-          ),
-      ],
+    return Semantics(
+      label: 'Scene Description',
+      child: Column(
+        children: [
+          for (var i = 0; i < (sceneDescription[0]).length; i++)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Semantics(
+                  label: '${sceneDescription[0][i]} object',
+                  child: Text(
+                    sceneDescription[0][i],
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Semantics(
+                  label: 'Distance: ${sceneDescription[1][i]} meters',
+                  child: Text(
+                    '${sceneDescription[1][i].toStringAsFixed(2)} m',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Semantics(
+                  label: 'Position: ${sceneDescription[2][i]}',
+                  child: Text(
+                    sceneDescription[2][i].toString(),
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+        ],
+      ),
     );
   }
 }
--- navi/lib/pages/navigation.dart
+++ navi/lib/pages/navigation.dart
@@ -112,7 +112,7 @@ class _NavigationState extends State<Navigation> {
                 // start again.
                 onPressed: stopNavigation_button_pressed,
                 // the server will stop getting api requests
-                child: const Text('Durdur hevra',
+                child: const Text('Stop Navigation',
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.w900,
